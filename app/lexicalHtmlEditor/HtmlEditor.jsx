658955b0e8323b28b6a61464d3389b9a6874be43
link <bjhhopesin@gmail.com>
2024-09-20 16:46:23 +0900

    first init

---
+++ app/lexicalHtmlEditor/HtmlEditor.jsx
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+
+import theme from "./thems/basicTheme";
+import ToolbarPlugin from "./plugins/ToolbarPlugin";
+import CustomToolbarPlugin from "./plugins/CustomToolbarPlugin";
+import TreeViewPlugin from "./plugins/TreeViewPlugin";
+
+const placeholder = "Enter some rich text...";
+
+const editorConfig = {
+  namespace: "React.js Demo",
+  nodes: [],
+  // Handling of errors during update
+  onError(error) {
+    throw error;
+  },
+  // The editor theme
+  theme,
+};
+
+export default function HtmlEditor() {
+  return (
+    <LexicalComposer initialConfig={editorConfig}>
+      <div className="editor-container">
+        <CustomToolbarPlugin />
+        <div className="editor-inner">
+          <RichTextPlugin
+            contentEditable={
+              <ContentEditable
+                className="editor-input"
+                aria-placeholder={placeholder}
+                placeholder={
+                  <div className="editor-placeholder">{placeholder}</div>
+                }
+              />
+            }
+            ErrorBoundary={LexicalErrorBoundary}
+          />
+          <HistoryPlugin />
+          <AutoFocusPlugin />
+          {/* <TreeViewPlugin /> */}
+        </div>
+      </div>
+    </LexicalComposer>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,5 +1,20 @@
 "use client";
 
+import HtmlEditor from "./lexicalHtmlEditor/HtmlEditor";
+
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <div>
+      <div
+        style={{
+          textAlign: "center",
+          marginTop: "20px",
+        }}
+      >
+        {" "}
+        lexical html editor
+      </div>
+      <HtmlEditor />
+    </div>
+  );
 }
